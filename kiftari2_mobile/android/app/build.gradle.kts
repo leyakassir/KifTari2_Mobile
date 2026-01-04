321d318af5306c1baeef1471a1dada0e521154ee
leyakassir <leya.kassir2@gmail.com>
2026-01-04 02:21:52 +0200

    Update Android minSdk to 24 and fix release build requirements

--- kiftari2_mobile/android/app/build.gradle.kts
+++ kiftari2_mobile/android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.example.kiftari2_mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 24
         targetSdk = 36 // FIX: align with updated plugin SDK requirement to remove warning
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -46,5 +46,5 @@ flutter {
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
